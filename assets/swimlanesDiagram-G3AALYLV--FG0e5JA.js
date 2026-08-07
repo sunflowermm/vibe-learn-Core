e64bfb15d19dbc59236eb2b5f3837017d491a2e1
vibe-learn-pages <pages@users.noreply.github.com>
2026-08-08 00:35:30 +0800

    deploy: vibe-learn static

---
+++ assets/swimlanesDiagram-G3AALYLV--FG0e5JA.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-CQWtLS3r.js";import{_ as a}from"./mermaid.core-BzNRJgct.js";import"./chunk-5VM5RSS4-Cv0djFUn.js";import"./chunk-XXDRQBXY-Bqro2tTW.js";import"./chunk-VR4S4FIN-0GKO56vN.js";import"./chunk-32BRIVSS-CwvGPtCt.js";import"./index-ByYIjMmn.js";import"./channel-DM5wAmYN.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
